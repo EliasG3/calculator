7b8476e5cb35e04935918fd80be3224f9ae68ef4
EliasG3 <el0016gu-s@student.lu.se>
2024-01-28 13:02:24 +0100

    fgh

--- src/main/java/com/models/App.java
+++ src/main/java/com/models/App.java
@@ -1,7 +1,38 @@
 package com.models;
 
-public class App {
+import java.net.URL;
+
+import com.controllers.MainViewController;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class App extends Application{
+
+    private Calculator calculator;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch();
     }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.calculator = new Calculator();
+        URL url = getClass().getResource("/fxml/MainView.fxml");
+        FXMLLoader loader = new FXMLLoader(url);
+        Scene scene = new Scene(loader.load());
+
+        MainViewController controller = loader.getController();
+        controller.setMainViewController(calculator);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Calculator");
+        primaryStage.show();
+    }
+
 }
